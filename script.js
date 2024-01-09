35d201bf22d25dec2d327b5e7913a9c8bfb7fc49
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2024-01-10 02:12:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function decimalToBinary(num) {
   // your code here
+	let binary = ''; 
+    while (num > 0) { 
+        binary = (num % 2) + binary; 
+        num = Math.floor(num / 2); 
+    } 
+    return binary; 
 }
 const num = parseInt(prompt("Enter a number."));
 alert(decimalToBinary(num));
